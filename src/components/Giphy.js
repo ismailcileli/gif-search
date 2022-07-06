e6a4104c096100eb014af15d7bb31b4ce3af6adf
ismailcileliiointel <ismlcll@hotmail.com>
2022-07-06 17:24:14 +0300

    style-fix

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -32,9 +32,83 @@ const Giphy = () => {
   const renderGifs = () => {
     return currentGifs.map((el) => {
       return (
-        <div key={el.id} className=" ml-2">
-          <img src={el.images.fixed_height_downsampled.url} />
-        </div>
+        // <div key={el.id} className=" ml-2">
+        //   <img src={el.images.fixed_height_downsampled.url} />
+        // </div>
+        <>
+          <div class="col-sm-4" key={el.id}>
+            <div class="card">
+              <div class="image">
+                <img src={el.images.fixed_height.url} />
+              </div>
+            </div>
+          </div>
+
+          {/* <div class="col-sm-4">
+            <div class="card">
+              <div class="image">
+               <img src={el.images.fixed_height.url} />
+              </div>
+              <div class="card-inner">
+                <div class="header">
+                  <h2>Title</h2>
+                  <h3>Sub-Head</h3>
+                </div>
+                <div class="content">
+                  <p>Content area</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="card">
+              <div class="image">
+               <img src={el.images.fixed_height.url} />
+              </div>
+              <div class="card-inner">
+                <div class="header">
+                  <h2>Title</h2>
+                  <h3>Sub-Head</h3>
+                </div>
+                <div class="content">
+                  <p>Content area</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="card">
+              <div class="image">
+             <img src={el.images.fixed_height.url} />
+              </div>
+              <div class="card-inner">
+                <div class="header">
+                  <h2>Title</h2>
+                  <h3>Sub-Head</h3>
+                </div>
+                <div class="content">
+                  <p>Content area</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="card">
+              <div class="image">
+            <img src={el.images.fixed_height.url} />
+              </div>
+              <div class="card-inner">
+                <div class="header">
+                  <h2>Title</h2>
+                  <h3>Sub-Head</h3>
+                </div>
+                <div class="content">
+                  <p>Content area</p>
+                </div>
+              </div>
+            </div>
+          </div> */}
+        </>
       );
     });
   };
